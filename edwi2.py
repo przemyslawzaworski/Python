fb983be5f2263ffa3e7d903ecea33016617cae65
przemyslawzaworski <przemyslawzaworski@gmail.com>
2017-10-06 15:45:54 +0200

    Second exercise.

---
+++ edwi2.py
@@ -0,0 +1,44 @@
+#author: Przemyslaw Zaworski
+#Python version 3.6.0
+
+import collections
+import urllib.request
+import os
+import re
+os.system('cls');
+
+def process_html(input):
+  s = re.compile('<.*?>')
+  output = re.sub(s, '', input) #delete HTML tags
+  output = re.sub(r'[^\w\s]','',output) #delete punctuation marks
+  return output.lower() #set all lower case
+
+print("Enter URL address: ");
+try:
+	url = input();
+	html = urllib.request.urlopen(url).read();
+	html_file = open("output.html", 'wb');
+	html_file.write(html);
+	html_file.close(); 
+except ValueError:
+	print ("Invalid URL address !");
+
+try:	
+	temp = open("output.html",'r'); 
+	s = temp.read() ;
+	pt = process_html(s);
+	txt_file = open("output.txt", 'w');
+	output_stream = str(pt.encode("windows-1250","ignore"));
+	txt_file.write(output_stream);
+	s =output_stream.split();
+	s.sort();
+	counter=collections.Counter(s);
+	print("Enter k number: ");
+	k=int(input());
+	n=sorted(counter.items(), key=lambda x: x[1],reverse=True);
+	for i in range(k):
+		print(n[i]);
+	print('##############################################');
+	txt_file.close();
+except ValueError:
+	print ("Could not create TXT file.");
